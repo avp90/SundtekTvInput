bbb4cb5b86c4716dba5684e52c2881c5b47ab235
Tamim Baschour <t.baschour@gmail.com>
2017-02-27 16:55:36 +0100

    Fix SundtekParser

---
+++ app/src/main/java/com/example/android/sampletvinput/SundtekParser/Model/SundtekChannel.java
@@ -0,0 +1,20 @@
+package com.example.android.sampletvinput.SundtekParser.Model;
+
+import org.json.JSONArray;
+
+/**
+ * Created by Tamim Baschour on 27.02.2017.
+ */
+
+public class SundtekChannel {
+
+
+    public SundtekChannel(JSONArray jsonArray) {
+    }
+
+    public String getChannelName() {
+        return null;
+    }
+}
+
+
---
+++ app/src/main/java/com/example/android/sampletvinput/SundtekParser/Model/SundtekProgram.java
@@ -0,0 +1,8 @@
+package com.example.android.sampletvinput.SundtekParser.Model;
+
+/**
+ * Created by Tamim Baschour on 27.02.2017.
+ */
+
+public class SundtekProgram {
+}

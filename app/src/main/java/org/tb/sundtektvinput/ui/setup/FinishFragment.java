87dd02a65d2655c4b4daf4c71e010c54613a8843
Tamim Baschour <t.baschour@gmail.com>
2017-03-10 00:28:23 +0100

    Remove placeholder drawable

--- app/src/main/java/org/tb/sundtektvinput/ui/setup/FinishFragment.java
+++ app/src/main/java/org/tb/sundtektvinput/ui/setup/FinishFragment.java
@@ -1,13 +1,11 @@
 package org.tb.sundtektvinput.ui.setup;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v17.leanback.widget.GuidanceStylist;
 import android.support.v17.leanback.widget.GuidedAction;
 import android.widget.Toast;
 
-import org.tb.sundtektvinput.R;
 import org.tb.sundtektvinput.ui.setup.base.SetupBaseFragment;
 
 import java.util.List;
@@ -20,8 +18,8 @@ public class FinishFragment extends SetupBaseFragment {
     public GuidanceStylist.Guidance onCreateGuidance(@NonNull Bundle savedInstanceState) {
         String title = "Scan finished";
         String description = "You are now ready to watch TV";
-        Drawable icon = getActivity().getDrawable(R.drawable.ic_launcher);
-        return new GuidanceStylist.Guidance(title, description, breadcrumb, icon);
+        // Drawable icon = getActivity().getDrawable(R.drawable.ic_launcher);
+        return new GuidanceStylist.Guidance(title, description, breadcrumb, null);
     }
 
     @Override
